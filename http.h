54bade7dbe356cb49c35a5a2c19a6dad87c6c727
Xuan Yang <xy91@duke.edu>
2019-02-18 22:02:21 -0500

    first commit: simple socket and head file

---
+++ http.h
@@ -0,0 +1,69 @@
+#include <arpa/inet.h>
+#include <mutex>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unordered_map>
+
+class HTTP {
+protected:
+  std::string buffer;
+  std::unordered_map<std::string, std::string> header;
+  std::string body;
+
+public:
+  virtual void parseBuffer() = 0;
+  virtual ~HTTP() {}
+};
+
+class HTTPRequest : public HTTP {
+private:
+  std::string method;
+  std::string url;
+
+public:
+  HTTPRequest(std::string temp) { buffer = temp; }
+
+  virtual void parseBuffer() {
+    // Read first line
+    size_t pos = buffer.find("\r\n");
+    if (pos != std::string::npos) {
+    }
+
+    // Read header
+
+    // Read body
+  }
+};
+
+class HTTPResponse {
+private:
+  int code;
+  std::string readon;
+};
+
+// BEGIN_REF - https://www.youtube.com/watch?v=ojOUIg13g3I&t=543s
+class MyLock {
+private:
+  std::mutex *mtx;
+
+public:
+  explicit MyLock(std::mutex *temp) {
+    temp->lock();
+    mtx = temp;
+  }
+
+  ~MyLock() { mtx->unlock(); }
+};
+// END_REF
+
+void handlehttp(int reqfd) {
+  // Get request
+  char buffer[40960];
+  int size = recv(reqfd, buffer, 40960, 0);
+  std::string temp(buffer);
+  HTTPRequest newreq(temp);
+
+  // Send request
+
+  // Send response
+}
---
+++ myproxy.cpp
@@ -0,0 +1,92 @@
+#include "http.h"
+#include <iostream>
+#include <mutex>
+#include <netdb.h> // gethostbyname
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <unistd.h>
+
+#define PORT 7654
+
+#define DEVELOPMENT 1
+
+std::mutex mymutex;
+
+int main(int argc, char **argv) {
+  // read command line parameters
+
+  // Create socket
+  int sockfd;
+  struct sockaddr_in proxyaddr;
+  socklen_t len = sizeof(proxyaddr);
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (DEVELOPMENT)
+    std::cout << "Sockfd: " << sockfd << std::endl;
+
+  // Get IP
+  char hostname[128];
+  if (gethostname(hostname, sizeof(hostname)) == -1) {
+    std::cout << "Hostname access fail" << std::endl;
+    exit(1);
+  }
+  struct hostent *hent;
+  hent = gethostbyname(hostname);
+  if (hent == NULL) {
+    std::cout << "Host info access fail" << std::endl;
+    exit(1);
+  }
+  if (DEVELOPMENT)
+    std::cout << "Hostname: " << hostname << std::endl;
+
+  char *serIP;
+  // get from h_addr
+  serIP = inet_ntoa(*(struct in_addr *)hent->h_addr);
+  if (DEVELOPMENT)
+    std::cout << "Proxy IP: " << serIP << std::endl;
+
+  // Initial memory address
+  memset(&proxyaddr, '\0', sizeof(proxyaddr));
+  proxyaddr.sin_family = AF_INET;
+  proxyaddr.sin_port = htons(PORT);
+  proxyaddr.sin_addr.s_addr = inet_addr(serIP);
+
+  // Bind socket to address:port
+  int ret = bind(sockfd, (struct sockaddr *)&proxyaddr, sizeof(proxyaddr));
+  if (ret < 0) {
+    std::cout << "Error in binding" << std::endl;
+    exit(1);
+  }
+
+  // Listening
+  if (listen(sockfd, 10) != 0) {
+    std::cout << "Error in listening" << std::endl;
+    exit(1);
+  }
+
+  while (1) {
+    // Get address info
+    struct sockaddr_in reqaddr;
+
+    // Accept request
+    int reqfd = accept(sockfd, (struct sockaddr *)&reqaddr, &len);
+    if (reqfd < 0) {
+      std::cout << "Error in accept" << std::endl;
+      exit(1);
+    }
+
+    if (DEVELOPMENT) {
+      std::cout << "Request from " << inet_ntoa(reqaddr.sin_addr) << std::endl;
+      exit(1);
+    }
+
+    // Create thread to handle request
+    MyLock lk(&mymutex);
+
+    handlehttp(reqfd);
+
+    close(reqfd);
+  }
+
+  return 0;
+}
